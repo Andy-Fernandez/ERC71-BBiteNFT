db5a617cd8c5aa14d3a68e90466c146bf531ecae
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2024-11-14 11:14:51 -0400

    El contrato de deploya y verfica correctamente en Sepolia, ahora tenemos un error con la red de Amoy y no sé que tipo de erro es pero el contrato si se verifica en PoligonScan pero no en OKLink

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,12 +7,17 @@ module.exports = {
     amoy: {
       url: process.env.AMOY_URL,
       accounts: [process.env.PRIVATE_KEY]
-    }
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
   },
   etherscan: {
     // Configura la API para PolygonScan
     apiKey: {
-      polygon: process.env.POLYGONSCAN_API_KEY
+      polygonAmoy: process.env.POLYGONSCAN_API_KEY,
+      sepolia: process.env.ETHERSCAN_API_KEY
     },
   },
 };
---
+++ scripts/deployAndVerify.js
@@ -0,0 +1,38 @@
+const { ethers, run } = require("hardhat");
+
+async function main() {
+  // Despliegue del contrato
+  const CuyCollectionNft = await ethers.getContractFactory("CuyCollectionNft");
+  const cuyCollectionNft = await CuyCollectionNft.deploy(
+    "Cuy Collection", // Nombre del token
+    "CUY"            // Símbolo del token
+  );
+
+  // Espera a que el contrato sea desplegado
+  await cuyCollectionNft.waitForDeployment();
+  console.log("Contrato desplegado en:", cuyCollectionNft.target);
+
+  // Espera para asegurar que el contrato esté disponible en la red
+  await new Promise(resolve => setTimeout(resolve, 60000)); // Espera 60 segundos
+
+  // Verificación del contrato
+  try {
+    await run("verify:verify", {
+      address: cuyCollectionNft.target,
+      constructorArguments: [
+        "Cuy Collection",
+        "CUY"
+      ],
+    });
+    console.log("Verificación exitosa");
+  } catch (error) {
+    console.error("Error en la verificación:", error);
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+// 0xfC810281e14B398679EA2ACC4eE74A042775Fb06
