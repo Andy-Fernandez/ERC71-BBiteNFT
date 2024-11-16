27732d65ebf8a6841f002f97a80df65fbe0bc835
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2024-11-15 20:05:05 -0400

    trabajando con Ethres 6, el script merkletree_2.js funciona bien

--- scripts/merkletree_2.js
+++ scripts/merkletree_2.js
@@ -1,27 +1,24 @@
 const { MerkleTree } = require("merkletreejs");
 const keccak256 = require("keccak256");
-const { ethers }  = require("ethers")
-const walletsAndIds = require("../utils/walletsAndIds.js");
-const { mergeAlias } = require("vite");
+const { ethers } = require("hardhat");
+const walletAndIds = require("../utils/walletAndIds");
 
 function hashToken(tokenId, account) {
-  try {
-    const checksumAddress = ethers.getAddress(account);
-    return ethers.solidityPackedKeccak256(
-      ["uint256", "address"],
-      [tokenId, checksumAddress]
-    ); // compatible con la version 6 de ethers.js
-  } catch (error){
-    console.error(`Error al hashear el token ${tokenId} y la cuenta ${account}`);
-    throw error;
-  }
-  
-}  // tenemos que hacer esto para que ethers.js incluya la función solidityPackedKeccak256, osea una función compatible con keccak256 de solidity
-
-function constryendoMerkeleTree() {
-  const elementosHasheados = walletsAndIds.map(({ tokenId, account }) => hashToken(tokenId, account));
-
-  console.log(elementosHasheados);
+    return Buffer.from(
+        ethers
+            .solidityPackedKeccak256(["uint256", "address"], [tokenId, account])
+            .slice(2),
+        "hex"
+    );
 }
 
-constryendoMerkeleTree();
+function construyendoMerkleTree() {
+    var elementosHasheados = walletAndIds.map(({ tokenId, account }) => {
+        return hashToken(tokenId, account);
+    });
+    merkleTree = new MerkleTree(elementosHasheados, keccak256, {
+        sortPairs: true,
+    });
+}
+
+construyendoMerkleTree();
